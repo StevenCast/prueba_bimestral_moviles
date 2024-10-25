e57c38bb3858e40eb064af58cde2d07553380765
unknown <sirpepega777@gmail.com>
2024-10-25 14:46:35 -0500

    actualización apartado fechas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,9 +7,25 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  fecha1: string;
+  fecha2: string;
+  dias: number | null=null;
+  constructor() {
+    this.fecha1='';
+    this.fecha2='';
+  }
+  calcularDias(){
+    if(this.fecha1 && this.fecha2){
+      const date1 = new Date(this.fecha1);
+      const date2 = new Date(this.fecha2);
 
-  
+      const diferencia = Math.abs(date2.getTime()-date1.getTime());
+
+      this.dias = Math.ceil(diferencia/(1000*3600*24));
+    }else{
+      this.dias = null
+    }
+  }
 
 
 
